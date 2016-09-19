53be697fe918df55d785ee40b66cebf3a7368419
bpamuk <bahar.pamuk@gmail.com>
2016-09-19 13:05:39 +0000

    Improve the README

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -4,3 +4,9 @@ class ApplicationController < ActionController::Base
     render html: "hello, world!"
   end
 end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+   def hello
+    render html: "hello, world!"
+  end
+end
