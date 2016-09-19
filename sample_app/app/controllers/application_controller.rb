a658a0075e6de8ce5d4c784634aa169baff7e972
bpamuk <bahar.pamuk@gmail.com>
2016-09-19 13:09:10 +0000

    Add hello

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,12 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-   def hello
-    render html: "hello, world!"
-  end
-end
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-   def hello
+
+  def hello
     render html: "hello, world!"
   end
 end
