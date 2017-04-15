d910fe4d220705fc641f40791c7ae1f8f7ad70eb
bpamuk <bahar.pamuk@gmail.com>
2017-04-15 23:07:09 +0000

    User sign up

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -7,21 +7,26 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
-  
-   test "should get about" do
-    get static_pages_about_url
+
+  test "should get about" do
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
-
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | #{@contact_title}"
+  end
 end
